763fc7479a55cf19912eade69a88dc634db33bca
Alessia <alexdeyn@gmail.com>
2022-06-08 20:01:44 +0200

    banana

--- src/pages/Pills/index.tsx
+++ src/pages/Pills/index.tsx
@@ -7,16 +7,19 @@ const { pills } = json;
 export default function Pills() {
   return (
     <div className="home-wrapper">
-      <Text>Pills</Text>
+      <Text textType='h3'>PILLS</Text>
+      <div className='pills-wrapper'>
       {pills.map(pill => {
         return (
-          <div key={pill} className='pill-container' style={{ maxWidth: '300px' }}>
+          
+          <div key={pill} className='pill-container'>
             <video width="100%" controls>
               <source src={`${process.env.REACT_APP_KUSAMA_BUCKET_PILLS}${pill}+PILL.mp4`} type="video/mp4" />
             </video>
           </div>
         )
       })}
+      </div>
     </div>
   );
 }
